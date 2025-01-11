1f1ca0de68801c68032b403a2f4c8aec0b98ec48
Santhosh NC <ncsanthosh7@gmail.com>
2025-01-11 20:36:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  backend "s3" {
-    bucket = "terraform-infosectrain-remote-backend"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-07c5ecd8498c59db5"
   instance_type = "t2.micro"
